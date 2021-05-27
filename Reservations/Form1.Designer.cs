e622fd384a273bfc4943d7253f9da3b1ced21ed3
jowkarm <jokar.mehdi2@gmail.com>
2021-05-26 18:01:42 -0700

    The methods are added

--- Reservations/Form1.Designer.cs
+++ Reservations/Form1.Designer.cs
@@ -45,120 +45,111 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(74, 29);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(106, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 15);
+            this.label1.Size = new System.Drawing.Size(106, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Arrival date:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtArrivalDate
             // 
-            this.txtArrivalDate.Location = new System.Drawing.Point(166, 28);
-            this.txtArrivalDate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtArrivalDate.Location = new System.Drawing.Point(237, 47);
             this.txtArrivalDate.Name = "txtArrivalDate";
-            this.txtArrivalDate.Size = new System.Drawing.Size(106, 23);
+            this.txtArrivalDate.Size = new System.Drawing.Size(150, 31);
             this.txtArrivalDate.TabIndex = 1;
+            this.txtArrivalDate.Tag = "Arrival date";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(54, 59);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(77, 98);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 15);
+            this.label2.Size = new System.Drawing.Size(135, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "Departure date:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtDepartureDate
             // 
-            this.txtDepartureDate.Location = new System.Drawing.Point(166, 57);
-            this.txtDepartureDate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDepartureDate.Location = new System.Drawing.Point(237, 95);
             this.txtDepartureDate.Name = "txtDepartureDate";
-            this.txtDepartureDate.Size = new System.Drawing.Size(106, 23);
+            this.txtDepartureDate.Size = new System.Drawing.Size(150, 31);
             this.txtDepartureDate.TabIndex = 2;
+            this.txtDepartureDate.Tag = "Departure date";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(38, 88);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(54, 147);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(104, 15);
+            this.label3.Size = new System.Drawing.Size(157, 25);
             this.label3.TabIndex = 4;
             this.label3.Text = "Number of nights:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtNights
             // 
-            this.txtNights.Location = new System.Drawing.Point(166, 86);
-            this.txtNights.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNights.Location = new System.Drawing.Point(237, 143);
             this.txtNights.Name = "txtNights";
             this.txtNights.ReadOnly = true;
-            this.txtNights.Size = new System.Drawing.Size(106, 23);
+            this.txtNights.Size = new System.Drawing.Size(150, 31);
             this.txtNights.TabIndex = 5;
             this.txtNights.TabStop = false;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(81, 118);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(116, 197);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 15);
+            this.label4.Size = new System.Drawing.Size(96, 25);
             this.label4.TabIndex = 6;
             this.label4.Text = "Total price:";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtTotalPrice
             // 
-            this.txtTotalPrice.Location = new System.Drawing.Point(166, 116);
-            this.txtTotalPrice.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTotalPrice.Location = new System.Drawing.Point(237, 193);
             this.txtTotalPrice.Name = "txtTotalPrice";
             this.txtTotalPrice.ReadOnly = true;
-            this.txtTotalPrice.Size = new System.Drawing.Size(106, 23);
+            this.txtTotalPrice.Size = new System.Drawing.Size(150, 31);
             this.txtTotalPrice.TabIndex = 7;
             this.txtTotalPrice.TabStop = false;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(28, 147);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(40, 245);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(114, 15);
+            this.label5.Size = new System.Drawing.Size(172, 25);
             this.label5.TabIndex = 8;
             this.label5.Text = "Avg. price per night:";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtAvgPrice
             // 
-            this.txtAvgPrice.Location = new System.Drawing.Point(166, 145);
-            this.txtAvgPrice.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAvgPrice.Location = new System.Drawing.Point(237, 242);
             this.txtAvgPrice.Name = "txtAvgPrice";
             this.txtAvgPrice.ReadOnly = true;
-            this.txtAvgPrice.Size = new System.Drawing.Size(106, 23);
+            this.txtAvgPrice.Size = new System.Drawing.Size(150, 31);
             this.txtAvgPrice.TabIndex = 9;
             this.txtAvgPrice.TabStop = false;
             // 
             // btnCalculate
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(51, 192);
-            this.btnCalculate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCalculate.Location = new System.Drawing.Point(73, 320);
             this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(78, 25);
+            this.btnCalculate.Size = new System.Drawing.Size(111, 42);
             this.btnCalculate.TabIndex = 3;
             this.btnCalculate.Text = "&Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(166, 192);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Location = new System.Drawing.Point(237, 320);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(78, 25);
+            this.btnExit.Size = new System.Drawing.Size(111, 42);
             this.btnExit.TabIndex = 4;
             this.btnExit.Text = "E&xit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -167,10 +158,10 @@
             // Form1
             // 
             this.AcceptButton = this.btnCalculate;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnExit;
-            this.ClientSize = new System.Drawing.Size(312, 238);
+            this.ClientSize = new System.Drawing.Size(446, 397);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.txtAvgPrice);
@@ -183,10 +174,10 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtArrivalDate);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reservations";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Reservations/Form1.cs
+++ Reservations/Form1.cs
@@ -22,10 +22,30 @@ namespace Reservations
             InitializeComponent();
         }
 
-        //public bool IsValidData()
-        //{
+        //The IsValidData method is added.
+        public bool IsValidData()
+        {
+            bool s = true;
+            List<bool> success = new List<bool>(6);
+            DateTime min = DateTime.Today;
+            DateTime max = min.AddYears(5);
 
-        //}
+            //Validate the arrival date text box
+            success.Add(IsPresent(txtArrivalDate, txtArrivalDate.Tag.ToString()));
+            success.Add(IsDateTime(txtArrivalDate, txtArrivalDate.Tag.ToString()));
+            success.Add(IsWithinRange(txtArrivalDate, txtArrivalDate.Tag.ToString(), min, max));
+
+            //Validate the departure date text box
+            success.Add(IsPresent(txtDepartureDate, txtDepartureDate.Tag.ToString()));
+            success.Add(IsDateTime(txtDepartureDate, txtDepartureDate.Tag.ToString()));
+            success.Add(IsWithinRange(txtDepartureDate, txtDepartureDate.Tag.ToString(), min, max));
+
+            if (success.Contains(false))
+            {
+                s = false;
+            }
+            return s;
+        }
 
         public bool IsPresent(TextBox textBox, string name)
         {
@@ -38,20 +58,127 @@ namespace Reservations
             return true;
         }
 
-        //public bool IsDateTime(TextBox textBox, string name)
-        //{
 
-        //}
+        //The IsDateTime method is added.
+        public bool IsDateTime(TextBox textBox, string name)
+        {
+            
+            if (!DateTime.TryParse(textBox.Text, out DateTime localDate))
+            {
+                MessageBox.Show(name + " must have a valid date format.",
+                    "Format Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
 
-        //public bool IsWithinRange(TextBox textBox, string name,
-        //    DateTime min, DateTime max)
-        //{
-
-        //}
+        //The IsWithinRange method is added.
+        public bool IsWithinRange(TextBox textBox, string name,
+            DateTime min, DateTime max)
+        {
+            if (DateTime.TryParse(textBox.Text, out DateTime localDate))
+            {
+                if (localDate < min || localDate > max)
+                {
+                    MessageBox.Show(name + " must be between " + min.ToShortDateString() + 
+                        " and " + max.ToShortDateString() + ".\n",
+                   "Range Error");
+                    textBox.Focus();
+                    return false;
+                }
+                   
+            }
+            return true;
+        }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //The IsValidData mehod is called.
+                if (IsValidData())
+                {
+                    //Get the arrival and departure date
+                    DateTime arrival = DateTime.Parse(txtArrivalDate.Text);
+                    DateTime departure = DateTime.Parse(txtDepartureDate.Text);
+
+                    //Check the departure date 
+                    if (arrival > departure)
+                    {
+                        MessageBox.Show("The departure date must be " +
+                            "after the arrival date.", "Entry Error");
+                    }
+
+
+                    //Calculate the number of days
+                    TimeSpan ts = departure.Subtract(arrival);
+                    int nights = ts.Days;
+
+                    //Calculate the total price
+                    int total = 0;
+
+                    while (arrival < departure)
+                    {
+                        //Find the day of week for pricing
+                        DayOfWeek dayofWeek = arrival.DayOfWeek;
+                        int price;
+                        if (dayofWeek == DayOfWeek.Friday ||
+                            dayofWeek == DayOfWeek.Saturday)
+                        {
+                            price = 150;
+                        }
+
+                        else
+                        {
+                            price = 120;
+                        }
+
+
+                        total += price;
+                        arrival = arrival.AddDays(1);
+                        continue;
+                    }
+
+
+
+
+                    decimal average = total / nights;
+
+                    //Display the results
+                    txtNights.Text = nights.ToString();
+                    txtAvgPrice.Text = average.ToString("c");
+                    txtTotalPrice.Text = total.ToString("c");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n\n" +
+                    ex.GetType().ToString() + "\n" +
+                    ex.StackTrace, "Exception");
+            }
+        }
+
+
+
+        //The Load event is added.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Show the current date for arrival date.
+            DateTime arrival = DateTime.Today;
+
+            //Show three days later as depurture date.
+            DateTime departure = arrival.AddDays(3);
+
+            //Format the dates
+            txtArrivalDate.Text = arrival.ToShortDateString();
+            txtDepartureDate.Text = departure.ToShortDateString();
+
         }
     }
 }
